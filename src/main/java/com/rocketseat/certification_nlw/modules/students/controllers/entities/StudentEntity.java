89c49aaf9aebb804bb3585d0ff4b91c788d5d12c
Rian Lima <rianlima235.rl@gmail.com>
2024-02-09 16:19:29 -0400

    Criando a tabela students

--- src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/entities/StudentEntity.java
@@ -3,6 +3,11 @@ package com.rocketseat.certification_nlw.modules.students.controllers.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +15,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 
 public class StudentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
-    private List<CertificationStudentsEntity> certificationStudentsEntity;
-    
+
+    //private List<CertificationStudentsEntity> certificationStudentsEntity;
 }
